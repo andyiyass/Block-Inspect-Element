984cf7341d362265df700f05fcedc5d6a61b98eb
andyiyass <37761526+andyiyass@users.noreply.github.com>
2023-12-06 00:10:01 +0700

    Update block.js

--- block.js
+++ block.js
@@ -1,22 +1,22 @@
-/* To Disable Inspect Element */
+	/* To Disable Inspect Element */
 	document.onkeydown = function(e) {
 	  if(event.keyCode == 123) {
-		 return window.location.replace("http://www.w3schools.com");
+		 return window.location.replace("https://www.google.com");
 	  }
 	  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
-		 return window.location.replace("http://www.w3schools.com");
+		 return window.location.replace("https://www.google.com");
 	  }
 	  if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
-		 return window.location.replace("http://www.w3schools.com");
+		 return window.location.replace("https://www.google.com");
 	  }
 	  if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
-		 return window.location.replace("http://www.w3schools.com");
+		 return window.location.replace("https://www.google.com");
 	  }
 	  if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
-		 return window.location.replace("http://www.w3schools.com");
+		 return window.location.replace("https://www.google.com");
 	  }
 		if(e.ctrlKey && e.keyCode == 'S'.charCodeAt(0)) {
-		 return window.location.replace("http://www.w3schools.com");
+		 return window.location.replace("https://www.google.com");
 	  }
 	}
 	
